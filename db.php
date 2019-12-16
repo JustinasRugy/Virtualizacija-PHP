13c376f4e739859ddbf63d7505f3b3098138ece3
JustinasRugys <58828461+JustinasRugys@users.noreply.github.com>
2019-12-16 03:08:16 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,17 @@
 <?php
-$con = mysqli_connect("localhost","root","","ligonine");
+
+$port = "193.219.91.103:";
+
+if($fh = fopen('DBport.txt', 'r')) {
+    while (!feof($fh)) {
+        $line = fgets($fh);
+
+    }
+    fclose($fh);
+}
+
+
+$con = mysqli_connect($port+$line,"root","","ligonine");
 if (mysqli_connect_error())
 {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
